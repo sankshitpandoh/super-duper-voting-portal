767ce147473f1dd386a79697fbaa2657747279ab
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-06-19 01:45:56 +0530

    conditional rendering on Admin.js

--- client/src/components/Admin/admin.js
+++ client/src/components/Admin/admin.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import '../stylesheets/Admin/admin.css';
 import LogIn from '../logIn.js';
+import AdminHome from './adminHome.js'; 
 
 class Admin extends React.Component{
+    state={
+        adminHome: false
+    }
 
-    checkCredentials = (username, password) =>{
-
-        /* making a POST request to server with username and password */
+    checkCredentials = async(username, password) => {
+        /* making a POST request to server to check if admin credentials are correct */
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username: x , password: y })
+            body: JSON.stringify({ username: username , password: password })
         };
         const response = await fetch('/adminLogin', requestOptions);
         let serverResponse = await response.json();
+        this.setState({
+            adminHome: serverResponse.adminPrivilege
+        })
     }
+    
     render(){
         return(
             <>
+                {this.state.adminHome ?
+                <AdminHome />
+                :
                 <LogIn checkCredentials={this.checkCredentials} />
+                }
             </>
         )
     }
---
+++ client/src/components/Admin/adminHome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class AdminHome extends React.Component{
+    render(){
+        return(
+            <h1>Admin home portal</h1>
+        )
+    }
+}
+
+export default AdminHome;
