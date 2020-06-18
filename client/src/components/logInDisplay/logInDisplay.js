904400b70504ad020fc58296008158398241c936
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-06-19 00:54:01 +0530

    added router and basic components

---
+++ client/src/components/logInDisplay/logInDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class LogInDisplay extends React.Component{
+    render(){
+        return(
+            <h1>LogInDisplay</h1>
+        )
+    }
+}
+
+export default LogInDisplay;
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home/home.js';
+import Admin from './components/Admin/admin.js';
+import LogInDisplay from './components/logInDisplay/logInDisplay.js';
 
 class App extends React.Component{
     render(){
         return(
-            <h1>hey</h1>
+            <BrowserRouter>
+            <div>
+                <Switch>
+                 <Route path="/" component={LogInDisplay} exact/>
+                 <Route path="/admin" component={Admin}/>
+                 <Route path="/home" component={Home}/>
+                <Route component={Error}/>
+               </Switch>
+            </div> 
+          </BrowserRouter>
         )
     }
 }
