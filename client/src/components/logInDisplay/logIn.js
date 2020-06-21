80e376173ca7d0e80aaf403c9d27cc74efc9a093
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-06-21 18:12:33 +0530

    log in page improvements

--- client/src/components/logInDisplay/logIn.js
+++ client/src/components/logInDisplay/logIn.js
@@ -4,7 +4,9 @@ import '../stylesheets/LogIn.css';
 class LogIn extends React.Component{
     state = {
         username: "",
-        password: ""
+        password: "",
+        logInError: false,
+        logInErrorMessage: ""
     }
 
     handleUserName = (e) => {
@@ -31,9 +33,29 @@ class LogIn extends React.Component{
                 })
              })()
              :
-             console.log("password field empty")
+             this.setState({
+                 logInError: true,
+                 logInErrorMessage: "Password field cannot be empty"
+             }, () => {
+                setTimeout( function(){
+                    this.setState({
+                        logInError: false,
+                        logInErrorMessage: ""
+                    })
+                }.bind(this), 3000)
+            })
              :
-             console.log("username field empty")
+             this.setState({
+                logInError: true,
+                logInErrorMessage: "Username field cannot be empty"
+            }, () => {
+                setTimeout( function(){
+                    this.setState({
+                        logInError: false,
+                        logInErrorMessage: ""
+                    })
+                }.bind(this), 3000)
+            })
              
     }
 
@@ -45,13 +67,19 @@ class LogIn extends React.Component{
                     Invalid Login Credentials
                 </div>
                 }
+                {
+                    this.state.logInError &&
+                    <div className="log-in-error d-flex p-1 justify-content-center">
+                        {this.state.logInErrorMessage}
+                    </div>
+                }
                 {this.props.hoverMenu &&
                     <div className="message-container d-flex justify-content-center p-1">
                             <p>Account sucessfully created, log in with your credentials</p>
                     </div>
                 }
-                <div className="container">
-                    <form className="d-flex flex-column align-items-center w-50 mx-auto" onSubmit={this.handleSubmit}>
+                <div className="log-in-container d-flex flex-column w-50 my-0 mx-auto p-5">
+                    <form className="d-flex flex-column align-items-center w-100 mx-auto" onSubmit={this.handleSubmit}>
                         <h2 className="mr-auto mb-3">Login Portal</h2>
                         <span className="d-flex flex-column w-100 mb-4">
                             Username:
@@ -61,7 +89,7 @@ class LogIn extends React.Component{
                             Password:
                             <input className="p-1" type="password" placeholder="Password here" value={this.state.password} onChange={this.handlePassword} />
                         </span>
-                        <input className="ml-auto mb-2" type="submit" value="Log In" />
+                        <input className="ml-auto mb-4 w-25 p-2 submit-button" type="submit" value="Log In" />
                         <span className="sign-up-message">Don't have an account? <strong onClick={this.props.switchingLogin}>Click here </strong> to SignUp</span>
                     </form>
 
